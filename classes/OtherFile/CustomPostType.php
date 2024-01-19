1e7e2aa9c1df6eaf2e7f15d4a50eff0b535447c0
Stephan Roloff <sr@werbeagenten.de>
2024-01-19 09:12:05 +0100

    fixed untracked files

--- classes/OtherFile/CustomPostType.php
+++ classes/OtherFile/CustomPostType.php
@@ -6,23 +6,23 @@ if(! class_exists('OtherNamespace\CustomPostType')){
     class CustomPostType {
 
         function __construct(){
-            add_action( 'init', array($this, 'custom_post_type_projects') );
+            add_action( 'init', array($this, 'custom_post_type_projectos') );
         }
 
-        function custom_post_type_projects() {
+        function custom_post_type_projectos() {
             // Set UI labels for Custom Post Type
                 $labels = array(
-                    'name'                => _x( 'Projects', 'Post Type General Name', 'default-theme' ),
-                    'singular_name'       => _x( 'Project', 'Post Type Singular Name', 'default-theme' ),
-                    'menu_name'           => __( 'Projects', 'default-theme' ),
-                    'parent_item_colon'   => __( 'Parent Project', 'default-theme' ),
-                    'all_items'           => __( 'All Projects', 'default-theme' ),
-                    'view_item'           => __( 'View Project', 'default-theme' ),
-                    'add_new_item'        => __( 'Add New Project', 'default-theme' ),
+                    'name'                => _x( 'Projectos', 'Post Type General Name', 'default-theme' ),
+                    'singular_name'       => _x( 'Projecto', 'Post Type Singular Name', 'default-theme' ),
+                    'menu_name'           => __( 'Projectos', 'default-theme' ),
+                    'parent_item_colon'   => __( 'Parent Projecto', 'default-theme' ),
+                    'all_items'           => __( 'All Projectos', 'default-theme' ),
+                    'view_item'           => __( 'View Projecto', 'default-theme' ),
+                    'add_new_item'        => __( 'Add New Projecto', 'default-theme' ),
                     'add_new'             => __( 'Add New', 'default-theme' ),
-                    'edit_item'           => __( 'Edit Project', 'default-theme' ),
-                    'update_item'         => __( 'Update Project', 'default-theme' ),
-                    'search_items'        => __( 'Search Project', 'default-theme' ),
+                    'edit_item'           => __( 'Edit Projecto', 'default-theme' ),
+                    'update_item'         => __( 'Update Projecto', 'default-theme' ),
+                    'search_items'        => __( 'Search Projecto', 'default-theme' ),
                     'not_found'           => __( 'Not Found', 'default-theme' ),
                     'not_found_in_trash'  => __( 'Not found in Trash', 'default-theme' ),
                 );
@@ -30,8 +30,8 @@ if(! class_exists('OtherNamespace\CustomPostType')){
             // Set other options for Custom Post Type
                 
                 $args = array(
-                    'label'               => __( 'Projects', 'default-theme' ),
-                    'description'         => __( 'Project news and reviews', 'default-theme' ),
+                    'label'               => __( 'Projectos', 'default-theme' ),
+                    'description'         => __( 'Projecto news and reviews', 'default-theme' ),
                     'labels'              => $labels,
                     // Features this CPT supports in Post Editor
                     'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
@@ -64,7 +64,7 @@ if(! class_exists('OtherNamespace\CustomPostType')){
                 );
                 
                 // Registering your Custom Post Type
-                register_post_type( 'Projects', $args );
+                register_post_type( 'Projectos', $args );
             
             }
     }
